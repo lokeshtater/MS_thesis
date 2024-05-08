f8238a36fef3ddc0a0cee2fe8b6d51d54a6516d6
lokesh <81503529+lokeshtater@users.noreply.github.com>
2024-05-08 12:40:42 +0530

    First Commit

---
+++ Operators.py
@@ -0,0 +1,16 @@
+from qutip import *
+
+#defining destruction operator for i-th site
+def single_destruct(m,N):
+    site_ops = []
+    for i in range(N):
+        if (i==m):
+            site_ops.append(destroy(2))
+        else:
+            site_ops.append(qeye(2))
+
+    return tensor(site_ops)
+
+def annihilation(N):
+    return [single_destruct(i,N) for i in range(N)]
+
