9d22ba5491a6a3647c9e78f5ee27c38bea23e2e9
Lokesh Tater <81503529+lokeshtater@users.noreply.github.com>
2024-06-03 17:49:12 +0530

    Code for numerical evolution of 2 and 4 point correlators

--- 2_4_pt_correlators.py
+++ 2_4_pt_correlators.py
@@ -1,10 +1,16 @@
 import numpy as np
 import pandas as pd
+import time
+from joblib import Parallel, delayed
+
 from qutip import *
+
+
 import Operators
 from scipy.integrate import solve_ivp
 
-N = 4 #number of lattice sites, sites labelled from 0 to N-1
+
+N = 12 #number of lattice sites, sites labelled from 0 to N-1
 a = Operators.annihilation(N) #list of single site annihilation operators for lattice of size N
 vacc =  tensor([basis(2,0) for i in range(N)]) #vaccum state
 
@@ -28,39 +34,48 @@ for n in range(N):
             for q in range(N):
                 F0[q*N**3 + p*N**2 + n*N + m] = expect(a[m].dag()*a[n].dag()*a[p]*a[q],init)
 
+print("Initial State Defined")
+
 #defining evolution matrices for 2-point and 4-point correlator vectors:
 A = np.zeros((N,N),dtype=complex)
 Id = np.zeros((N,N),dtype=complex)
 
 for i in range(N):
     for j in range(N):
-        if(i==j):
-            Id[i][j] = 1.0 #also defining identity matrix
-
-        #This is for tight binding, changes for long range hopping
-        if (i==((j+1)%N)): 
-            A[i][j] = -1.0j
+        if(i==j):  Id[i][j] = 1.0 #also defining identity matrix
+        if (i==((j+1)%N) | i==((j-1)%N) ):  A[i][j] = -1.0j
+       
 
 B = -A
 D_evol = np.kron(Id,A) + np.kron(B,Id)
 F_evol = np.kron(Id,np.kron(Id,np.kron(Id,A))) +  np.kron(Id,np.kron(Id,np.kron(A,Id))) +  np.kron(Id,np.kron(B,np.kron(Id,Id))) +  np.kron(B,np.kron(Id,np.kron(Id,Id)))
 
+print("Evolution Matrices Defined")
+
 ####Solving for evolution of 2 and 4 point correlator vectors- D and F#############
 ###################################################################################
-
-def ode_equation(t,y,matrix): #function to define ODE
-    return np.dot(matrix,y)
-
-t_span = (0,100)
-t_eval = np.linspace(0,100,1000)
-
 
 #Select which ODE to solve for
 y0 = D0   
 evol_matrix = D_evol
 
+
+def ode_func(t,y): #function to define ODE
+    return np.dot(evol_matrix,y)
+    
+    
+t_span = (0,10**5)
+
+
+
+print("Solving ODE...")
 #solve ODE
-solution = solve_ivp(ode_equation, t_span, y0, t_eval=t_eval, args=(evol_matrix,))
+start_time = time.time()
+solution = solve_ivp(ode_func, t_span, y0, t_eval=t_eval, args=(evol_matrix,))
+end_time = time.time()
+print(f"ODE solved. Time taken:{end_time-start_time}")
+
+
 
 t_points = solution.t
 y_points = solution.y.T  # Transpose to get a 2D array with time points along rows
@@ -72,6 +87,8 @@ df.index.name = 'time'
 # Save the DataFrame to a CSV file
 df.to_csv('./SR_data/alt_d.csv')
 
+
+print("DONE!")
 print(df.head())  # Print the first few rows to verify
 
 
