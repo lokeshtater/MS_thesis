8dfa77235ecacf57b0fa69ed0ebd5daea9bd7c55
Lokesh Tater <81503529+lokeshtater@users.noreply.github.com>
2024-06-12 17:28:09 +0530

    system type, size and initial state taken as input

--- correlators_direct.py
+++ correlators_direct.py
@@ -3,46 +3,43 @@ import time
 from scipy.linalg import expm
 import cmath
 import pandas as pd 
+import sys
 
-N = 125 #number of lattice sites, sites labelled from 0 to N-1
+N = int(sys.argv[1]) #number of lattice sites, sites labelled from 0 to N-1
+system = sys.argv[2]
+state = sys.argv[3]
 
-
+print(f"{N} {state}")
 #initial states for 2-point and 4-point correlator vectors:
 
 D0 = np.zeros(N**2,dtype=complex) #vector to store initial state
 #F0 = np.zeros(N**4,dtype=complex)
 
-for n in range(N):
-    for m in range(N):
-        if(n==m):
-           if((n+1)%2==0):
-               D0[N*n + m] = 1.0 + 0.0j     #alternating pure state 
-          #if(n<int(N/2)):             #domain wall state 
-            #    D0[N*n + m] = 1.0 + 0.0j      
-            # if(n<int(N/2)):                  #staggered state
-            #     if((n+1)%2==0):
-            #         D0[N*n + m] = 1.0 + 0.0j
-        
+
+if(state=="alt"): ind = [(N*n + n) for n in range(N) if (n+1)%2==0]
+if(state=="dom"): ind = [(N*n + n) for n in range(int(N/2))]
+if(state=="stagg"): ind = [(N*n + n) for n in range(int(N/2)) if (n+1)%2==0]
+
+D0[ind] = 1.0 + 0.0j
+
 print("Initial State Defined")
 
 #defining evolution matrices for 2-point and 4-point correlator vectors:
 A = np.zeros((N,N),dtype=complex)
 Id = np.zeros((N,N),dtype=complex)
 
-#tight-binding
-# for i in range(N):
-#     for j in range(N):
-#         if(i==j):  Id[i][j] = 1.0 #also defining identity matrix
-#         if (abs(i-j)==1):  A[i][j] = -1.0j
-# A[0][N-1] = A[N-1][0] = -1.0j #periodicity of lattice
 
-#long range hopping: #closed boundaries
-alpha = 1.4
+if(system=="tight_binding"):
+    for i in range(N):
+        Id[i][i] = 1.0
+        A[i][int((i+1)%N)] = A[i][i-1] = -1.0j
 
-for i in range(N):
-    Id[i][i] = 1.0
-    for j in range(i+1,N):
-        A[i][j] = A[j][i] = (1/(j-i)**alpha)*1.0j
+if(system=="long_hopping"):
+    alpha = float(sys.argv[4])
+    for i in range(N):
+        Id[i][i] = 1.0
+        for j in range(i+1,N):
+            A[i][j] = A[j][i] = (1/(j-i)**alpha)*1.0j
 
 B = -A
 
@@ -72,9 +69,10 @@ for i in range(len(times)):
     
 df1 = pd.DataFrame(data=D, index=times)
 df1.index.name = 'time'
-df1.to_csv(f'./data_SR/long_hopping/2_pt_alt_{N}_sites.csv')
+df1.to_csv(f'./data_SR/{system}/2_pt_{state}_{N}_sites.csv')
 
 
 print(df1.head())
+print(alpha)
 print("DONE!")
 
